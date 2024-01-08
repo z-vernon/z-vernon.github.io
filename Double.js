023f66bba859ace84dc1ce9e794b32176831dcb4
z-vernon <wzvernon@gmail.com>
2024-01-07 19:05:06 -0500

    Update Double.js

--- Double.js
+++ Double.js
@@ -8,16 +8,9 @@ var mass_2 = 10;
 var arm_length_1 = 120;
 var arm_length_2 = 120;
 var Run = true;
-function toggle_Run(){
-	background(255);
-	tracer.background(255);
-	Run = ! Run;
 
-		
-	
-}
 //Constants
-var gravity = 1; // Gravity is set to 1 because code breaks when 9.8 m/s^2 is used. 
+var gravity = 0.25; // Gravity is set to 1 because code breaks when 9.8 m/s^2 is used. 
 var radius = 25;
 var previous_point_x_2 = -1;
 var previous_point_y_2 = -1
@@ -28,13 +21,12 @@ var tracer;
 
 function setup() {
   // This creates the space for the graphics to be displayed
-  
   var canvas = createCanvas(700, 370);
   canvas.parent('doublecode');
 
   // Set angles with PI which is global and therefore must be defined in setup.
-  angle_1 = PI / 2;
-  angle_2 = PI / 2;
+  angle_1 = PI ;
+  angle_2 = 3.14 ;
   
   // Set the origin to fit the canvas
   origin_x = width / 2;
@@ -44,7 +36,7 @@ function setup() {
   tracer = createGraphics(width, height);
   
   // Set background to write
-  tracer.background(255);
+  tracer.background(0);
   
   // Set origin of tracer object to the origin
   tracer.translate(origin_x, origin_y);
@@ -83,32 +75,40 @@ function draw() {
   
  // Converting to cartesian coordinates from polar for pendulum 2
   var position_x_2 = position_x_1 + arm_length_2 * sin(angle_2);
-  var position_y_2 = position_y_1 + arm_length_2 * cos(angle_2);  
+  var position_y_2 = position_y_1 + arm_length_2 * cos(angle_2);
+ 
+  // Visualization of origin
+
+  fill(255);
+  ellipse(0,0, radius, radius);
+
   // Visualization of Pendulum 1
+  stroke(255);
+  strokeWeight(10)
   line(0, 0, position_x_1, position_y_1);
-  fill(0);
+  fill(255);
   ellipse(position_x_1, position_y_1, radius, radius);
   
   // Visualization of Pendulum 2
+  stroke(255);
+  strokeWeight(10)
   line(position_x_1, position_y_1, position_x_2, position_y_2);
-  fill(0);
+  fill(255);
   ellipse(position_x_2, position_y_2, radius, radius);
   
   // Computing angular velocity
   angular_velocity_1 += angular_acceleration_1;
   angular_velocity_2 += angular_acceleration_2;
-  
-  // This is connected the run button
- 
+   
   // Computing angle
   angle_1 += angular_velocity_1;
   angle_2 += angular_velocity_2;
   
   
   // traces the path of the bottom oendulum
-  if (frameCount > 1) {
+  /*if (frameCount > 1) {
     tracer.line(previous_point_x_2, previous_point_y_2, position_x_2, position_y_2);
-  }
+  }*/
   
   // records the previous position information for next cycle.
   previous_point_x_2 = position_x_2;
