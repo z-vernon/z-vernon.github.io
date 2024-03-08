1096b8c379b4f1eb3ce4fe8d304d0a6ca4f829d2
z-vernon <wzvernon@gmail.com>
2024-03-08 15:33:50 -0500

    Update Double.js

--- Double.js
+++ Double.js
@@ -87,7 +87,7 @@ function draw() {
   strokeWeight(5);
   noFill();
   stroke(255);
-  circle(0, 0, radius_circle*4.1275);
+  circle(0, 0, radius_circle*4.1325);
 
   fill(255);
   ellipse(0,0, 1, 1);
