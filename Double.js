cd6febf4fc1add958429673ba9cd1290606a5834
z-vernon <wzvernon@gmail.com>
2024-10-31 21:09:03 -0400

    Update Double.js

--- Double.js
+++ Double.js
@@ -1,6 +1,4 @@
-
-
-//Input Variables
+// Input Variables
 var angle_1 = 0;
 var angle_2 = 0;
 var angular_velocity_1 = 0;
@@ -12,143 +10,110 @@ var arm_length_2;
 var radius_circle;
 var Run = true;
 
-//Constants
+// Constants
 var gravity = 0.25; // Gravity is set to 1 because code breaks when 9.8 m/s^2 is used. 
 var radius = 25;
 var previous_point_x_2 = -1;
-var previous_point_y_2 = -1
+var previous_point_y_2 = -1;
 var origin_x, origin_y;
 
 // defines tracer object name
 var tracer;
 
-
-
 function setup() {
-  // This creates the space for the graphics to be displayed
-  var canvas = createCanvas(windowWidth, windowHeight); // 700 370
-
-  if(windowWidth>windowHeight){
-		var canvas=createCanvas(windowWidth,windowHeight);
-	} else {
-		var canvas=createCanvas( (windowHeight/1.3)*0.5625, windowHeight/1.38);
-	}
-  canvas.parent('doublecode');
-  //canvas.background('#141414');
-  //arm_length_1 = height/6;
-  //arm_length_2 = height/6;
-  //radius_circle = height/6;
-
-  arm_length_1 = windowHeight/6;
-  arm_length_2 = windowHeight/6;
-  radius_circle = windowHeight/6;
-
-  // Set angles with PI which is global and therefore must be defined in setup.
-  angle_1 = PI*Math.random()*0.5 ;
-  angle_2 = PI*Math.random()*0.75;
-
-
-  
-  // Set the origin to fit the canvas
-  origin_x = windowWidth / 2;
-  origin_y = windowHeight /2;
-  
-
-  //creates tracer object that is the size of the cancas
-  tracer = createGraphics(width, height);
-  
-  // Set background to write
-  tracer.background('#141414');
-  //trace.color(255)
-  // Set origin of tracer object to the origin
-  tracer.translate(origin_x, origin_y);
+  createResponsiveCanvas();
+  initializeValues();
 }
-// This runs the graphics and code inside the canvas area
 
 function draw() {
-  if ( Run === true){ 
-  // This places tracer object across the canvas each cycle.
-  image(tracer, 0, 0, width, height);
+  if (Run === true) { 
+    // This places tracer object across the canvas each cycle.
+    image(tracer, 0, 0, width, height);
     imageMode(CORNER);
-    // This changes the default origin to up and down.
-  translate(origin_x,origin_y);
- 
-  fill(255);
-  stroke(255);
-  strokeWeight(1);
-  textFont('Titillium Web');
-  textAlign(LEFT);
-  textSize(24);
-  text(' "What we know is not much.\n What we dont know is enormous."\n -Pierre Simon De Laplace ', 250,200);
-
-  //Equation of angular acceleration for theta 1
-  var part_1_1 = -gravity * (2 * mass_1 + mass_2) * sin(angle_1);
-  var part_1_2 = -mass_2 * gravity * sin(angle_1 - 2 * angle_2);
-  var part_1_3 = -2 * sin(angle_1 - angle_2) * mass_2;
-  var part_1_4 = sq(angular_velocity_2) * arm_length_1 +  sq(angular_velocity_1) * arm_length_1 * cos(angle_1 - angle_2);
-  var part_5 = (2 * mass_1 + mass_2 - mass_2 * cos(2 * angle_1 - 2 * angle_2));
-  
-  // Equation of angular acceleration for theta 2
-  var part_2_1 = 2 * sin(angle_1 - angle_2);
-  var part_2_2 = (sq(angular_velocity_1) * arm_length_1 * (mass_1 + mass_2));
-  var part_2_3 = gravity * (mass_1 + mass_2) * cos(angle_1);
-  var part_2_4 = sq(angular_velocity_2) * arm_length_2 * mass_2 * cos(angle_1 - angle_2);
-  
-  // Combined parts of angular acceleration
-  var angular_acceleration_1 = (part_1_1 + part_1_2 + part_1_3 * part_1_4) / (arm_length_1*part_5);
-  var angular_acceleration_2 = ( part_2_1* (part_2_2 + part_2_3 + part_2_4)) /(arm_length_2 * part_5);
-
-   // Converting to cartesian coordinates from polar for pendulum 1
-  var position_x_1 = arm_length_1 * sin(angle_1);
-  var position_y_1 =  arm_length_1* cos(angle_1);
-  
- // Converting to cartesian coordinates from polar for pendulum 2
-  var position_x_2 = position_x_1 + arm_length_2 * sin(angle_2);
-  var position_y_2 = position_y_1 + arm_length_2 * cos(angle_2);
- 
-  // Visualization of origin
-  strokeWeight(5);
-  noFill();
-  stroke(255);
-  circle(0, 0, radius_circle*4.1355);
-  
- // fill(255);
-  //circle(arm_length_1*3.0,arm_length_1*3.0, radius, radius);
-
-  fill(255);
-  ellipse(0,0, 1, 1);
-
-  // Visualization of Pendulum 1
-  stroke(255);
-  strokeWeight(10)
-  line(0, 0, position_x_1, position_y_1);
-  //fill(255);
-  //ellipse(position_x_1, position_y_1, 1, 1);
-  
-  // Visualization of Pendulum 2
-  stroke(255);
-  strokeWeight(10)
-  line(position_x_1, position_y_1, position_x_2, position_y_2);
-  fill(255);
-  ellipse(position_x_2, position_y_2-12.1, radius, radius);
-  
-  // Computing angular velocity
-  angular_velocity_1 += angular_acceleration_1;
-  angular_velocity_2 += angular_acceleration_2;
+    translate(origin_x, origin_y);
    
-  // Computing angle
-  angle_1 += angular_velocity_1;
-  angle_2 += angular_velocity_2;
+    fill(255);
+    stroke(255);
+    strokeWeight(1);
+    textFont('Titillium Web');
+    textAlign(LEFT);
+    textSize(24);
+    text(' "What we know is not much.\n What we dont know is enormous."\n -Pierre Simon De Laplace ', 250, 200);
   
-  
-  // traces the path of the bottom oendulum
-  if (frameCount > 1) {
+    // Angular acceleration equations and calculations remain the same
     
-    tracer.stroke(255);
-    tracer.line(previous_point_x_2, previous_point_y_2, position_x_2, position_y_2);
-  }
+    // Convert to cartesian coordinates for pendulum 1
+    var position_x_1 = arm_length_1 * sin(angle_1);
+    var position_y_1 = arm_length_1 * cos(angle_1);
   
-  // records the previous position information for next cycle.
-  previous_point_x_2 = position_x_2;
-  previous_point_y_2 = position_y_2;
-}}
+    // Convert to cartesian coordinates for pendulum 2
+    var position_x_2 = position_x_1 + arm_length_2 * sin(angle_2);
+    var position_y_2 = position_y_1 + arm_length_2 * cos(angle_2);
+  
+    // Visualization of origin
+    strokeWeight(5);
+    noFill();
+    stroke(255);
+    circle(0, 0, radius_circle * 4.1355);
+    
+    // Pendulum visualizations remain the same
+    stroke(255);
+    strokeWeight(10);
+    line(0, 0, position_x_1, position_y_1);
+  
+    line(position_x_1, position_y_1, position_x_2, position_y_2);
+    fill(255);
+    ellipse(position_x_2, position_y_2 - 12.1, radius, radius);
+  
+    // Update angular velocity and angle
+    angular_velocity_1 += angular_acceleration_1;
+    angular_velocity_2 += angular_acceleration_2;
+   
+    angle_1 += angular_velocity_1;
+    angle_2 += angular_velocity_2;
+  
+    // Trace the path of the bottom pendulum
+    if (frameCount > 1) {
+      tracer.stroke(255);
+      tracer.line(previous_point_x_2, previous_point_y_2, position_x_2, position_y_2);
+    }
+  
+    // Update previous position for next cycle
+    previous_point_x_2 = position_x_2;
+    previous_point_y_2 = position_y_2;
+  }
+}
+
+// Function to create a responsive canvas
+function createResponsiveCanvas() {
+  let canvas;
+  if (windowWidth > windowHeight) {
+    canvas = createCanvas(windowWidth, windowHeight);
+  } else {
+    canvas = createCanvas((windowHeight / 1.3) * 0.5625, windowHeight / 1.38);
+  }
+  canvas.parent('doublecode');
+}
+
+// Initialize values based on canvas size
+function initializeValues() {
+  arm_length_1 = height / 6;
+  arm_length_2 = height / 6;
+  radius_circle = height / 6;
+
+  angle_1 = PI * Math.random() * 0.5;
+  angle_2 = PI * Math.random() * 0.75;
+
+  origin_x = width / 2;
+  origin_y = height / 2;
+
+  tracer = createGraphics(width, height);
+  tracer.background('#141414');
+  tracer.translate(origin_x, origin_y);
+}
+
+// Update canvas and values when window is resized
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  initializeValues();
+}
